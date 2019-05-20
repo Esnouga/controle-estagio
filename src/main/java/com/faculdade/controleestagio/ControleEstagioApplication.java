d2c11bbeb4e6edc96c99c6288cad44e1a8ae2c74
Alessandro Corbanesi <alessandrocorbanesi@gmail.com>
2019-05-20 20:11:00 -0300

    init

---
+++ src/main/java/com/faculdade/controleestagio/ControleEstagioApplication.java
@@ -0,0 +1,13 @@
+package com.faculdade.controleestagio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleEstagioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleEstagioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/faculdade/controleestagio/ControleEstagioApplicationTests.java
@@ -0,0 +1,16 @@
+package com.faculdade.controleestagio;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ControleEstagioApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
